220c230d4ff5113b52e9977f9948a71092a04289
Tadelin <tadelindeleon10@gmail.com>
2019-10-29 17:56:31 -0400

    Completed review

--- session-10-29/session-10-29-review.py
+++ session-10-29/session-10-29-review.py
@@ -1,23 +1,27 @@
 # Welcome back!
+import numpy as np
 
-# Reviewing While loops:
+
 # Write code to print multiples of 5 from 45 to 90
-
+i=9
+while i<=18:
+	print(i*5)
+	i+=1
 
 # With your partner: discuss: What is a set? What is it good for?
 # Write your response as a comment here:
-#
+#It is good for storing data. It is an unordered list that stores data.
 
 # Create a variable called "myset" and put whatever you want in it:
-
+myset = {"hello", 5, 4}
 
 
 # Create a 2D numpy array of size 3x5 with numbers 1-5:
-
+a = np.array([(1,2,3,4,5), (6,7,8,9,10), (11,12,13,14,15)])
 
 
 # From your array, print the number 7:
-
+print(a[1,1])
 
 
 # Challenge: use while loop to print the following pattern:
@@ -25,6 +29,9 @@
 # ***
 # **
 # *
-
+i = "****"
+while len(1)>=1:
+	print(i)
+	len(i-1)
 
 
