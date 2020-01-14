1587f9d2e1c587764d16cf2af7852fafc65d92fc
Tadelin <tadelindeleon10@gmail.com>
2020-01-14 17:34:03 -0500

    1/14/20

--- session-12-10/tensorflow_tutorial.py
+++ session-12-10/tensorflow_tutorial.py
@@ -52,15 +52,15 @@ model.compile(optimizer='adam',
 history = model.fit(train_images, train_labels, epochs=10, 
                     validation_data=(test_images, test_labels))
 
-#plots the loss/accuracy of the model.
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
+# #plots the loss/accuracy of the model.
+# plt.plot(history.history['accuracy'], label='accuracy')
+# plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.ylim([0.5, 1])
+# plt.legend(loc='lower right')
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-print(test_acc)
+# test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+# print(test_acc)
 
 
