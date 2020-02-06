5910d92de319f3a1cd13d8fd5bf6572d41b9f59a
Tadelin <tadelindeleon10@gmail.com>
2020-02-06 17:25:31 -0500

    febuary

--- session-1-14/amnh_data.py
+++ session-1-14/amnh_data.py
@@ -1,3 +1,4 @@
+#importing all libraries needed for program
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import warnings  
@@ -15,7 +16,7 @@ with warnings.catch_warnings():
 
 
 print('ready') 
-
+#Makes the function show_batch to print the images and gives the label.
 def show_batch(image_batch, label_batch):
   plt.figure(figsize=(10,10))
   print(label_batch.shape)
@@ -28,23 +29,25 @@ def show_batch(image_batch, label_batch):
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-data_dir = '/Users/helenfellow/Documents/Education /cnn_data'
+#shows the path taken to get to the image
+data_dir = '/Users/brownscholar/Desktop/pictures'
 data_dir = pathlib.Path(data_dir)
 
+#gets the amount of images 
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
-
+#Makes a class name for animal
 CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != ".DS_Store"])
 print(CLASS_NAMES)
 
-seal_1 = list(data_dir.glob('seal_1/*'))
+Porcupine = list(data_dir.glob('Porcupine/*'))
 
 
 image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-BATCH_SIZE = 622
-IMG_HEIGHT = 1024
-IMG_WIDTH = 768
+BATCH_SIZE = 256
+IMG_HEIGHT = 4032
+IMG_WIDTH = 3024
 STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
 
 
@@ -56,17 +59,17 @@ train_data_gen = image_generator.flow_from_directory(directory=str(data_dir),
                                                      class_mode="sparse")
 
 image_batch, label_batch = next(train_data_gen)
-show_batch(image_batch, label_batch)
+#show_batch(image_batch, label_batch)
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(1024, 768, 3)))
+model.add(layers.Conv2D(32, (10, 10), activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH, 3)))
+model.add(layers.MaxPooling2D((40, 40)))
+model.add(layers.Conv2D(32, (20, 20), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(2, activation='softmax'))
 
 print(model.summary())
 
