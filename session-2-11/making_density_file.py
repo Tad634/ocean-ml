4e0e68de34f674f3a3240530e8bd4531bc538ed4
Tadelin <tadelindeleon10@gmail.com>
2020-02-13 16:46:37 -0500

    2/13/20

--- session-2-11/making_density_file.py
+++ session-2-11/making_density_file.py
@@ -1,2 +1,25 @@
 # to do: 
+from netCDF4 import Dataset
+import numpy as np
+
+#input: salinity, temperature, and pressure in form of netcdf file 
+#1st: import data and get all 
+dataset = Dataset(r'/Users/brownscholar/Desktop/dataset-armor-3d-rep-weekly_1574699840388.nc')
+salinity= dataset['so']
+temp = dataset['to']
+pressure = dataset['depth']
+latitude = dataset['latitude']
+longitude = dataset['longitude']
+
+print(salinity.shape)
+print(temp.shape)
+print(pressure.shape)
+print(latitude.shape)
+print(longitude.shape)
+
+pressure3D = np.zeroes((31,80,27))
+
+for i in pressure:
+	pressure3D[1,:,:]
+
 
