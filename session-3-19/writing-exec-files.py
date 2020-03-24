e409f6fa0280485a2ea2f0ec5d5a9fa329b64436
Tadelin <tadelindeleon10@gmail.com>
2020-03-24 16:40:07 -0400

    3/24/20

--- session-3-19/writing-exec-files.py
+++ session-3-19/writing-exec-files.py
@@ -1,1 +1,8 @@
 # to do
+from netCDF4 import Dataset
+import numpy
+import datetime as td
+dataset= Dataset(r"/Users/brownscholar/Desktop/dataset-armor-3d-rep-weekly_1574699840388.nc")
+time = dataset['time']
+time_one = str(time[1])
+print(time_one)
