93009dd84054c797f3ce6b9e0246544dd6d439cf
Tadelin <tadelindeleon10@gmail.com>
2020-04-28 16:55:25 -0400

    hovmoller plot start

--- session-4-21/hovmoller-plot-start.py
+++ session-4-21/hovmoller-plot-start.py
@@ -4,4 +4,23 @@ import numpy as np
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
+dataset = Dataset(r'/Users/brownscholar/Desktop/fortran_files/omega.nc')
+w_values = dataset["w"]
+print(w_values.shape)
 
+lat = dataset["latitude"]
+first_plot = w_values[:,0,:,0]
+first_plot = np.swapaxes(first_plot,0,1)
+print(first_plot.shape)
+
+top = cm.get_cmap('Blues_r')
+bottom = cm.get_cmap('Reds')
+newcolors = np.vstack((top(np.linspace(0, 1, 10)),
+                       bottom(np.linspace(0, 1, 10))))
+newcmp = ListedColormap(newcolors, name='RedBlue')
+plt.title("hovmoller diagram at latitude = " + str(lat[0]))
+plt.xlabel('years')
+plt.ylabel('longitude')
+plt.pcolormesh(first_plot,cmap=newcmp)
+plt.colorbar()
+plt.show()
